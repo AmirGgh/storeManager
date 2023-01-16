6fad4bbc2a6126e3c7ffed23b25ad8731388d848
AmirGgh <am.g.gith@gmail.com>
2023-01-16 12:13:24 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: "#2C3333",
+      main: "#395B64",
+      light: "#A5C9CA",
+      bright: "#FEFCF3",
+      gray: "#F1F6F5",
+      red: "#DD5353",
+      green: "#82CD47",
+    },
+  },
+  typography: {
+    fontSize: 12,
+  },
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </CssBaseline>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
